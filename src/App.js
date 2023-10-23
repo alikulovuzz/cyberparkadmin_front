b10faae5022c2e3f2ff941190bda3eb00f4ab7ad
alikulovuzz <azamat.aliqulov@list.ru>
2023-10-23 11:39:25 +0500

    add delete button

--- src/App.js
+++ src/App.js
@@ -33,6 +33,7 @@ import QuarterlyReport from "./components/QuarterlyReport";
 import Auditing from "./components/Auditing";
 import MonthlyReport from "./components/MonthlyReport";
 import NewQuarterly from "./components/NewQuarterly";
+import { ErrorBoundary } from 'react-error-boundary';
 
 const App = () => {
   const [theme, colorMode] = useMode();
@@ -40,47 +41,48 @@ const App = () => {
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-          <UserProvider >
-            <Routes>
-              {
-                // sessionStorage.getItem("access_token") ? (
-                  <Route path="/" element={<MainComponent/>}>
-                    <Route path="admin" element={<Admin />}>
-                      <Route index element={<Dashboard />} />
-                      <Route path="team" element={<Team />} />
-                      <Route path="contacts" element={<Contacts />} />
-                      <Route path="invoices" element={<Invoices />} />
-                      <Route path="form" element={<Form />} />
-                      <Route path="bar" element={<Bar />} />
-                      <Route path="pie" element={<Pie />} />
-                      <Route path="line" element={<Line />} />
-                      <Route path="faq" element={<FAQ />} />
-                      <Route path="calendar" element={<Calendar />} />
-                      <Route path="geography" element={<Geography />} />
+        <ErrorBoundary fallback={<p>Serverda no'malum xatolik. Iltimos keyinroq qaytatdan harakat qilib ko'ring!</p>}><UserProvider >
+          <Routes>
+            {
+              // sessionStorage.getItem("access_token") ? (
+              <Route path="/" element={<MainComponent />}>
+                <Route path="admin" element={<Admin />}>
+                  <Route index element={<Dashboard />} />
+                  <Route path="team" element={<Team />} />
+                  <Route path="contacts" element={<Contacts />} />
+                  <Route path="invoices" element={<Invoices />} />
+                  <Route path="form" element={<Form />} />
+                  <Route path="bar" element={<Bar />} />
+                  <Route path="pie" element={<Pie />} />
+                  <Route path="line" element={<Line />} />
+                  <Route path="faq" element={<FAQ />} />
+                  <Route path="calendar" element={<Calendar />} />
+                  <Route path="geography" element={<Geography />} />
+                </Route>
+                <Route path="user" element={<UserMain />}>
+                  <Route index element={<Navigate to="company" replace={true} />} />
+                  <Route path="company" element={<MyCompany />} />
+                  <Route path="application" element={<Application />} />
+                  <Route path="reports" element={<Reports />} >
+                    <Route index element={<Navigate to="quarterly" replace={true} />} />
+                    <Route path="quarterly" element={<Outlet />} >
+                      <Route index element={<QuarterlyReport />} />
+                      <Route path="new-quarterly" element={<NewQuarterly />} />
                     </Route>
-                    <Route path="user" element={<UserMain/>}>
-                      <Route index element={<Navigate to="company" replace={true} />} />
-                      <Route path="company" element={<MyCompany />} />
-                      <Route path="application" element={<Application />} />
-                      <Route path="reports" element={<Reports />} >
-                        <Route index element={<Navigate to="quarterly" replace={true} />} />
-                        <Route path="quarterly" element={<Outlet />} >
-                          <Route index element={<QuarterlyReport />} />
-                          <Route path="new-quarterly" element={<NewQuarterly />} />
-                        </Route>
-                        <Route path="auditing" element={<Auditing />} />
-                        <Route path="monthly" element={<MonthlyReport />} />
-                      </Route>
-                    </Route>
+                    <Route path="auditing" element={<Auditing />} />
+                    <Route path="monthly" element={<MonthlyReport />} />
                   </Route>
-                // ):
-                // <></>
-              }
-              <Route path="login" element={<SignInSide admin={false}/>} />
-              <Route path="admin-login" element={<SignInSide admin={true}/>} />
-              {/* <Route path="*" element={<Navigate to={sessionStorage.getItem("access_token") ? "/" : "login"} />} /> */}
-            </Routes>
-          </UserProvider>
+                </Route>
+              </Route>
+              // ):
+              // <></>
+            }
+            <Route path="login" element={<SignInSide admin={false} />} />
+            <Route path="admin-login" element={<SignInSide admin={true} />} />
+            {/* <Route path="*" element={<Navigate to={sessionStorage.getItem("access_token") ? "/" : "login"} />} /> */}
+          </Routes>
+        </UserProvider>
+        </ErrorBoundary>
       </ThemeProvider>
     </ColorModeContext.Provider>
   );
