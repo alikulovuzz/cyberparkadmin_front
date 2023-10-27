966c1bfe80b141e5bf1233ce02278b8b19705b1a
alikulovuzz <azamat.aliqulov@list.ru>
2023-10-27 10:24:46 +0500

    choraklik hisbot add

--- src/components/AuditCompany/index.jsx
+++ src/components/AuditCompany/index.jsx
@@ -6,13 +6,22 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import { Toaster, toast } from "sonner";
-import Dialog from "@mui/material/Dialog";
 import Button from "@mui/material/Button";
-import DialogActions from "@mui/material/DialogActions";
-import DialogTitle from "@mui/material/DialogTitle";
-import Slide from "@mui/material/Slide";
 import Paper from "@mui/material/Paper";
 import { Box, Pagination } from "@mui/material";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Slide from "@mui/material/Slide";
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+} from "@mui/material";
 import PageSize from "../PageSize";
 import { postRequest, deleteReports } from "../../utils/resquests";
 import { getlist_v2 } from "../../utils/API_urls";
@@ -25,9 +34,11 @@ export default function AuditCompany() {
   const [page, setPage] = useState(1);
   const [compantList, setCompoundList] = useState([]);
   const [pageCount, setPageCount] = useState(1);
+  const [status, setStatus] = React.useState("progress");
   const [pageSize, setPageSize] = useState(10);
   const [a, forceUpdate] = useReducer((x) => x + 1, 0);
   const [open, setOpen] = React.useState(false);
+  const [openstatus, setOpenstatus] = React.useState(false);
   const [id, setID] = React.useState();
 
   const [audit, setAudit] = useState("Audit");
@@ -45,11 +56,35 @@ export default function AuditCompany() {
       });
     handleClose();
   };
+  const handleChangeStatus = (data) => {
+    postRequest(`audit/status_change`, {
+        report_id: id,
+        status: status,
+      }
+    )
+      .then((response) => {
+        toast.success("Muvaffaqiyatli o'chrildi!");
+        forceUpdate();
+      })
+      .catch((error) => {
+        console.log(error)
+        toast.error("Server xatolik");
+        forceUpdate();
+      });
+    handleClose();
+  };
+  const handleChange = (event) => {
+    setStatus(event.target.value);
+  };
   const handleClose = () => {
     setOpen(false);
+    setOpenstatus(false);
   };
   const handleOpen = () => {
     setOpen(!open);
+  };
+  const handleOpenStatus = () => {
+    setOpenstatus(!open);
   };
 
   useEffect(() => {
@@ -67,7 +102,7 @@ export default function AuditCompany() {
       .catch((error) => {
         console.log(error);
       });
-  }, [page, pageSize,a]);
+  }, [page, pageSize, a]);
 
   return (
     <>
@@ -93,14 +128,29 @@ export default function AuditCompany() {
                 sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
               >
                 <TableCell component="th" scope="row">
-                  {row.company_id.organization_name?row.company_id.organization_name:row.company_id}
+                  {row.company_id.organization_name
+                    ? row.company_id.organization_name
+                    : row.company_id}
                 </TableCell>
                 <TableCell align="right">{row.quarterly}</TableCell>
                 <TableCell align="right">{row.company_id.pinfl}</TableCell>
                 <TableCell align="right">{row.year}</TableCell>
-                <TableCell align="right">{row.status}</TableCell>
-                <TableCell align="right">{new Date(row.createdAt).toLocaleDateString("en-GB")}</TableCell>
-                <TableCell align="right">{new Date(row.createdAt).toLocaleTimeString("en-GB")}</TableCell>
+                <TableCell
+                  align="right"
+                  onClick={() => {
+                    // handelDelete(data._id);
+                    setID(row._id);
+                    handleOpenStatus();
+                  }}
+                >
+                  {row.status}
+                </TableCell>
+                <TableCell align="right">
+                  {new Date(row.createdAt).toLocaleDateString("en-GB")}
+                </TableCell>
+                <TableCell align="right">
+                  {new Date(row.createdAt).toLocaleTimeString("en-GB")}
+                </TableCell>
                 {/* <TableCell align="right">
                   <a href={row.file_link} download>
                     Yuklab olish
@@ -130,12 +180,43 @@ export default function AuditCompany() {
         onClose={handleClose}
         aria-describedby="alert-dialog-slide-description"
       >
-        <DialogTitle>
-          {"Hisobotni o'chirishni tasdiqlaysizmi?"}
-        </DialogTitle>
+        <DialogTitle>{"Hisobotni o'chirishni tasdiqlaysizmi?"}</DialogTitle>
         <DialogActions>
           <Button onClick={handleClose}>Yo'q</Button>
           <Button onClick={handelDelete}>Ha</Button>
+        </DialogActions>
+      </Dialog>
+      <Dialog
+        open={openstatus}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={handleClose}
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogTitle>{"Hisbotni holatini uzagrtirish"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-slide-description" sx={{ p: 1 }}>
+            <FormControl fullWidth sx={{ my: 1 }}>
+              <InputLabel id="demo-simple-select-label">Chorak</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={status}
+                label="Chorak"
+                // size='small'
+                onChange={handleChange}
+              >
+                <MenuItem value={"not_in_progress"}>Yuborildi</MenuItem>
+                <MenuItem value={"progress"}>Ko'rilmoqda</MenuItem>
+                <MenuItem value={"finished"}>Tasdiqlandi</MenuItem>
+                <MenuItem value={"disabled"}>Rad etildi</MenuItem>
+              </Select>
+            </FormControl>
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Yo'q</Button>
+          <Button onClick={handleChangeStatus}>Ha</Button>
         </DialogActions>
       </Dialog>
       <Toaster key={123} richColors position="bottom-right" />
--- src/components/QuarterlyCompany/index.jsx
+++ src/components/QuarterlyCompany/index.jsx
@@ -6,28 +6,39 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import { Toaster, toast } from "sonner";
-import Paper from "@mui/material/Paper";
-import Dialog from "@mui/material/Dialog";
 import Button from "@mui/material/Button";
+import Paper from "@mui/material/Paper";
+import { Box, Pagination } from "@mui/material";
+import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
 import Slide from "@mui/material/Slide";
-import { Box, Pagination } from "@mui/material";
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+} from "@mui/material";
 import PageSize from "../PageSize";
 import { postRequest, deleteReports } from "../../utils/resquests";
 import { getlist_v2 } from "../../utils/API_urls";
 
 const Transition = React.forwardRef(function Transition(props, ref) {
-    return <Slide direction="up" ref={ref} {...props} />;
-  });
+  return <Slide direction="up" ref={ref} {...props} />;
+});
 
 export default function QuarterlyCompany() {
   const [page, setPage] = useState(1);
   const [compantList, setCompoundList] = useState([]);
   const [pageCount, setPageCount] = useState(1);
+  const [status, setStatus] = React.useState("progress");
   const [pageSize, setPageSize] = useState(10);
   const [a, forceUpdate] = useReducer((x) => x + 1, 0);
   const [open, setOpen] = React.useState(false);
+  const [openstatus, setOpenstatus] = React.useState(false);
   const [id, setID] = React.useState();
 
   const [audit, setAudit] = useState("Choraklik");
@@ -45,11 +56,35 @@ export default function QuarterlyCompany() {
       });
     handleClose();
   };
+  const handleChangeStatus = (data) => {
+    postRequest(`audit/status_change`, {
+        report_id: id,
+        status: status,
+      }
+    )
+      .then((response) => {
+        toast.success("Muvaffaqiyatli o'chrildi!");
+        forceUpdate();
+      })
+      .catch((error) => {
+        console.log(error)
+        toast.error("Server xatolik");
+        forceUpdate();
+      });
+    handleClose();
+  };
+  const handleChange = (event) => {
+    setStatus(event.target.value);
+  };
   const handleClose = () => {
     setOpen(false);
+    setOpenstatus(false);
   };
   const handleOpen = () => {
     setOpen(!open);
+  };
+  const handleOpenStatus = () => {
+    setOpenstatus(!open);
   };
 
   useEffect(() => {
@@ -67,7 +102,7 @@ export default function QuarterlyCompany() {
       .catch((error) => {
         console.log(error);
       });
-  }, [page, pageSize,a]);
+  }, [page, pageSize, a]);
 
   return (
     <>
@@ -93,14 +128,29 @@ export default function QuarterlyCompany() {
                 sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
               >
                 <TableCell component="th" scope="row">
-                  {row.company_id.organization_name}
+                  {row.company_id.organization_name
+                    ? row.company_id.organization_name
+                    : row.company_id}
                 </TableCell>
                 <TableCell align="right">{row.quarterly}</TableCell>
                 <TableCell align="right">{row.company_id.pinfl}</TableCell>
                 <TableCell align="right">{row.year}</TableCell>
-                <TableCell align="right">{row.status}</TableCell>
-                <TableCell align="right">{new Date(row.createdAt).toLocaleDateString("en-GB")}</TableCell>
-                <TableCell align="right">{new Date(row.createdAt).toLocaleTimeString("en-GB")}</TableCell>
+                <TableCell
+                  align="right"
+                  onClick={() => {
+                    // handelDelete(data._id);
+                    setID(row._id);
+                    handleOpenStatus();
+                  }}
+                >
+                  {row.status}
+                </TableCell>
+                <TableCell align="right">
+                  {new Date(row.createdAt).toLocaleDateString("en-GB")}
+                </TableCell>
+                <TableCell align="right">
+                  {new Date(row.createdAt).toLocaleTimeString("en-GB")}
+                </TableCell>
                 {/* <TableCell align="right">
                   <a href={row.file_link} download>
                     Yuklab olish
@@ -130,12 +180,43 @@ export default function QuarterlyCompany() {
         onClose={handleClose}
         aria-describedby="alert-dialog-slide-description"
       >
-        <DialogTitle>
-          {"Hisobotni o'chirishni tasdiqlaysizmi?"}
-        </DialogTitle>
+        <DialogTitle>{"Hisobotni o'chirishni tasdiqlaysizmi?"}</DialogTitle>
         <DialogActions>
           <Button onClick={handleClose}>Yo'q</Button>
           <Button onClick={handelDelete}>Ha</Button>
+        </DialogActions>
+      </Dialog>
+      <Dialog
+        open={openstatus}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={handleClose}
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogTitle>{"Hisbotni holatini uzagrtirish"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-slide-description" sx={{ p: 1 }}>
+            <FormControl fullWidth sx={{ my: 1 }}>
+              <InputLabel id="demo-simple-select-label">Chorak</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={status}
+                label="Chorak"
+                // size='small'
+                onChange={handleChange}
+              >
+                <MenuItem value={"not_in_progress"}>Yuborildi</MenuItem>
+                <MenuItem value={"progress"}>Ko'rilmoqda</MenuItem>
+                <MenuItem value={"finished"}>Tasdiqlandi</MenuItem>
+                <MenuItem value={"disabled"}>Rad etildi</MenuItem>
+              </Select>
+            </FormControl>
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Yo'q</Button>
+          <Button onClick={handleChangeStatus}>Ha</Button>
         </DialogActions>
       </Dialog>
       <Toaster key={123} richColors position="bottom-right" />
