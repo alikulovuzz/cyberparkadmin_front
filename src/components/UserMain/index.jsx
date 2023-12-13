2940e7f80ca30265100b48b47643d1b4ca4bcfdc
alikulovuzz <azamat.aliqulov@list.ru>
2023-12-13 16:26:51 +0500

    logic changed to normal

--- src/components/UserMain/index.jsx
+++ src/components/UserMain/index.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useContext } from 'react'
 import { UserContext } from '../../context/UserContext'
 import UserWrapper from '../UserWrapper'
 
 export default function UserMain() {
-  const { user } = useContext(UserContext)
-  
+  const { user, setUser } = useContext(UserContext);
+
+  useEffect(() => {
+    if (!user?.role.includes("company")) {
+      sessionStorage.clear();
+      setUser("");
+      window.location.reload(false);
+    }
+  }, [user]);
   return (
     <div>
       {/* {user?.organization_name} */}
