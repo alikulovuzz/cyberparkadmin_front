2b07bfbf4d0e8b0487df51f445931a9996128a3d
alikulovuzz <azamat.aliqulov@list.ru>
2023-12-15 17:24:09 +0500

    add logic to fill application form

--- src/components/ApplyResidents/index.jsx
+++ src/components/ApplyResidents/index.jsx
@@ -7,9 +7,10 @@ import { FormControl, TextField } from "@mui/material";
 import { postRequest, uploadFile } from "../../utils/resquests";
 import { Toaster, toast } from "sonner";
 import { upload } from "../../utils/API_urls";
+import SuccessMessage from "../SuccessMessage";
 
 export default function ApplyResidents() {
-  const [data, setData] = useState({});
+  const [statusResult, setStatusResult] = useState(true);
   const [value, setValue] = React.useState(null);
 
 
@@ -113,12 +114,16 @@ export default function ApplyResidents() {
   const handleSubmit = async (event) => {
     event.preventDefault();
     setValidate(true)
-    uploadFiles()
+    await uploadFiles()
+    console.log("body")
+    console.log(body)
     postRequest("/application_form/create", body)
       .then((response) => {
         toast.success("Muvaffaqiyatli!");
         // handleClose();
         clearInputs()
+        setValidate(false)
+        setStatusResult(false)
         // navigate({
         //   pathname: "/user",
         // });
@@ -130,154 +135,154 @@ export default function ApplyResidents() {
       });  
   };
 
-  return (
-    <>
-      <div className="container-scroller">
-        <div className="custom-title-form">
-          <div className="card-style-form mb-30">
-            <div>
-              <h4 className="mb-25 pt-50">
-                ЗАЯВКА НА РЕЗИДЕНТСВО В IT-PARK ИННОВАЦИЙ В КИБЕРНЕТИКЕ
-              </h4>
+  return (statusResult?(<>
+    <div className="container-scroller">
+      <div className="custom-title-form">
+        <div className="card-style-form mb-30">
+          <div>
+            <h4 className="mb-25 pt-50">
+              ЗАЯВКА НА РЕЗИДЕНТСВО В IT-PARK ИННОВАЦИЙ В КИБЕРНЕТИКЕ
+            </h4>
+          </div>
+          <div className="card-style mb-30">
+            <div style={{ width: "50%", lineHeight: "10px" }}>
+              <h5 className="mb-25">
+                В данной форме представлен полный перечень документов для
+                подачи заявки на резидентство в IT-PARK ИННОВАЦИЙ В
+                КИБЕРНЕТИКЕ *
+              </h5>
             </div>
-            <div className="card-style mb-30">
-              <div style={{ width: "50%", lineHeight: "10px" }}>
-                <h5 className="mb-25">
-                  В данной форме представлен полный перечень документов для
-                  подачи заявки на резидентство в IT-PARK ИННОВАЦИЙ В
-                  КИБЕРНЕТИКЕ *
-                </h5>
+            <FormControl onSubmit={handleSubmit}>
+              <div className="apply-resident">
+                <TextField
+                  key="input_app"
+                  label="Электронная почта"
+                  variant="outlined"
+                  placeholder="Электронная почта *"
+                  className="apply-block"
+                  value={email}
+                  onChange={handleEmailChange}
+                />
+                {!(validate&&!email)?(<></>):(<><div className="warn-file">Пожалуйста, заполните вышеуказанные файлы...</div></>)}
+                <MuiFileInput
+                  label="Основные требования для получения статуса резидента (Файл для ознакомления №1)"
+                  placeholder="Файл *"
+                  className="apply-block"
+                  value={requirements}
+                  onChange={(file) => handleChangeFile("requirements", file)}
+                />
+                {!(validate&&!requirements)?(<></>):(<><div className="warn-file">Пожалуйста, заполните вышеуказанные файлы...</div></>)}
+                <MuiFileInput
+                  label="Заявление о регистрации в качестве резидента (Файл №2)"
+                  placeholder="Файл *"
+                  className="apply-block"
+                  value={application}
+                  onChange={(file) => handleChangeFile("application", file)}
+                />
+                {!(validate&&!application)?(<></>):(<><div className="warn-file">Пожалуйста, заполните вышеуказанные файлы...</div></>)}
+                <MuiFileInput
+                  label="Копии учредительных документов: Устав, Учредительный договор (если учредителей больше одного), Гувохнома."
+                  placeholder="Файл *"
+                  className="apply-block"
+                  value={constituent_documents}
+                  onChange={(file) =>
+                    handleChangeFile("constituent_documents", file)
+                  }
+                />
+                {!(validate&&!constituent_documents)?(<></>):(<><div className="warn-file">Пожалуйста, заполните вышеуказанные файлы...</div></>)}
+                <MuiFileInput
+                  label="Описание проекта, предлагаемого для реализации в качестве резидента IТ-парка инноваций в кибернетике (оригинал с живой подписью и печатью)"
+                  placeholder="Файл *"
+                  className="apply-block"
+                  value={description}
+                  onChange={(file) => handleChangeFile("description", file)}
+                />
+                {!(validate&&!description)?(<></>):(<><div className="warn-file">Пожалуйста, заполните вышеуказанные файлы...</div></>)}
+                <MuiFileInput
+                  label="Копия лицензии или разрешительного документа (при наличии)."
+                  placeholder="Файл *"
+                  className="apply-block"
+                  value={license}
+                  onChange={(file) => handleChangeFile("license", file)}
+                />
+                {!(validate&&!license)?(<></>):(<><div className="warn-file">Пожалуйста, заполните вышеуказанные файлы...</div></>)}
+                <MuiFileInput
+                  label="Копия приказа на директора и его паспорта"
+                  placeholder="Файл *"
+                  className="apply-block"
+                  value={copy_passport}
+                  onChange={(file) => handleChangeFile("copy_passport", file)}
+                />
+                {!(validate&&!copy_passport)?(<></>):(<><div className="warn-file">Пожалуйста, заполните вышеуказанные файлы...</div></>)}
+                <MuiFileInput
+                  label="Примерная форма описания проекта (Файл №3)"
+                  placeholder="Файл *"
+                  value={project_description}
+                  className="apply-block"
+                  onChange={(file) =>
+                    handleChangeFile("project_description", file)
+                  }
+                />
+                {!(validate&&!project_description)?(<></>):(<><div className="warn-file">Пожалуйста, заполните вышеуказанные файлы...</div></>)}
+                <MuiFileInput
+                  label="Анкета кандидата (Файл №4)"
+                  placeholder="Файл *"
+                  className="apply-block"
+                  value={candidate_application}
+                  onChange={(file) =>
+                    handleChangeFile("candidate_application", file)
+                  }
+                />
+                {!(validate&&!candidate_application)?(<></>):(<><div className="warn-file">Пожалуйста, заполните вышеуказанные файлы...</div></>)}
+                <MuiFileInput
+                  label="Структура бизнес план (Файл №5)"
+                  placeholder="Файл *"
+                  className="apply-block"
+                  value={business_plan}
+                  onChange={(file) => handleChangeFile("business_plan", file)}
+                />
+                {!(validate&&!business_plan)?(<></>):(<><div className="warn-file">Пожалуйста, заполните вышеуказанные файлы...</div></>)}
+                <div className="apply-div">
+                  <p>
+                    Указ президента, где Вы можете ознакомиться с льготами
+                    (УП-5099-сон 30.06.2017. О мерах по коренному улучшению
+                    условий для развития отрасли информационных технологий в
+                    республике)
+                    <a href="https://lex.uz/docs/3249654"> <br></br>
+                    Ссылка 1
+                    </a>
+                  </p>
+                </div>
+                <div className="apply-div">
+                  <p>
+                    Закон о кибербезопасности, который был подписан недавно{" "}
+                    <br></br>
+                    <a href="https://lex.uz/ru/docs/5960609?query=%D0%BA%D0%B8%D0%B1%D0%B5%D1%80%D0%B1%D0%B5%D0%B7%D0%BE%D0%BF%D0%B0%D1%81%D0%BD%D0%BE%D1%81%D1%82%D1%8C#sr-1 ">
+                    Ссылка 2
+                    </a>
+                  </p>
+                </div>
+                <div className="apply-div">
+                  <p>
+                    Если Вам нужно будет проходить сертификацию в Центре
+                    Кибербезопасности, то для наших резидентов есть льготы в
+                    этом плане. Мы предоставляем резидентам услугу подготовки
+                    к сертификации в виде экспертизы исходных кодов.
+                  </p>
+                </div>
               </div>
-              <FormControl onSubmit={handleSubmit}>
-                <div className="apply-resident">
-                  <TextField
-                    key="input_app"
-                    label="Электронная почта"
-                    variant="outlined"
-                    placeholder="Электронная почта *"
-                    className="apply-block"
-                    value={email}
-                    onChange={handleEmailChange}
-                  />
-                  {!(validate&&!email)?(<></>):(<><div className="warn-file">Пожалуйста, заполните вышеуказанные файлы...</div></>)}
-                  <MuiFileInput
-                    label="Основные требования для получения статуса резидента (Файл для ознакомления №1)"
-                    placeholder="Файл *"
-                    className="apply-block"
-                    value={requirements}
-                    onChange={(file) => handleChangeFile("requirements", file)}
-                  />
-                  {!(validate&&!requirements)?(<></>):(<><div className="warn-file">Пожалуйста, заполните вышеуказанные файлы...</div></>)}
-                  <MuiFileInput
-                    label="Заявление о регистрации в качестве резидента (Файл №2)"
-                    placeholder="Файл *"
-                    className="apply-block"
-                    value={application}
-                    onChange={(file) => handleChangeFile("application", file)}
-                  />
-                  {!(validate&&!application)?(<></>):(<><div className="warn-file">Пожалуйста, заполните вышеуказанные файлы...</div></>)}
-                  <MuiFileInput
-                    label="Копии учредительных документов: Устав, Учредительный договор (если учредителей больше одного), Гувохнома."
-                    placeholder="Файл *"
-                    className="apply-block"
-                    value={constituent_documents}
-                    onChange={(file) =>
-                      handleChangeFile("constituent_documents", file)
-                    }
-                  />
-                  {!(validate&&!constituent_documents)?(<></>):(<><div className="warn-file">Пожалуйста, заполните вышеуказанные файлы...</div></>)}
-                  <MuiFileInput
-                    label="Описание проекта, предлагаемого для реализации в качестве резидента IТ-парка инноваций в кибернетике (оригинал с живой подписью и печатью)"
-                    placeholder="Файл *"
-                    className="apply-block"
-                    value={description}
-                    onChange={(file) => handleChangeFile("description", file)}
-                  />
-                  {!(validate&&!description)?(<></>):(<><div className="warn-file">Пожалуйста, заполните вышеуказанные файлы...</div></>)}
-                  <MuiFileInput
-                    label="Копия лицензии или разрешительного документа (при наличии)."
-                    placeholder="Файл *"
-                    className="apply-block"
-                    value={license}
-                    onChange={(file) => handleChangeFile("license", file)}
-                  />
-                  {!(validate&&!license)?(<></>):(<><div className="warn-file">Пожалуйста, заполните вышеуказанные файлы...</div></>)}
-                  <MuiFileInput
-                    label="Копия приказа на директора и его паспорта"
-                    placeholder="Файл *"
-                    className="apply-block"
-                    value={copy_passport}
-                    onChange={(file) => handleChangeFile("copy_passport", file)}
-                  />
-                  {!(validate&&!copy_passport)?(<></>):(<><div className="warn-file">Пожалуйста, заполните вышеуказанные файлы...</div></>)}
-                  <MuiFileInput
-                    label="Примерная форма описания проекта (Файл №3)"
-                    placeholder="Файл *"
-                    value={project_description}
-                    className="apply-block"
-                    onChange={(file) =>
-                      handleChangeFile("project_description", file)
-                    }
-                  />
-                  {!(validate&&!project_description)?(<></>):(<><div className="warn-file">Пожалуйста, заполните вышеуказанные файлы...</div></>)}
-                  <MuiFileInput
-                    label="Анкета кандидата (Файл №4)"
-                    placeholder="Файл *"
-                    className="apply-block"
-                    value={candidate_application}
-                    onChange={(file) =>
-                      handleChangeFile("candidate_application", file)
-                    }
-                  />
-                  {!(validate&&!candidate_application)?(<></>):(<><div className="warn-file">Пожалуйста, заполните вышеуказанные файлы...</div></>)}
-                  <MuiFileInput
-                    label="Структура бизнес план (Файл №5)"
-                    placeholder="Файл *"
-                    className="apply-block"
-                    value={business_plan}
-                    onChange={(file) => handleChangeFile("business_plan", file)}
-                  />
-                  {!(validate&&!business_plan)?(<></>):(<><div className="warn-file">Пожалуйста, заполните вышеуказанные файлы...</div></>)}
-                  <div className="apply-div">
-                    <p>
-                      Указ президента, где Вы можете ознакомиться с льготами
-                      (УП-5099-сон 30.06.2017. О мерах по коренному улучшению
-                      условий для развития отрасли информационных технологий в
-                      республике)
-                      <a href="https://lex.uz/docs/3249654"> <br></br>
-                      Ссылка 1
-                      </a>
-                    </p>
-                  </div>
-                  <div className="apply-div">
-                    <p>
-                      Закон о кибербезопасности, который был подписан недавно{" "}
-                      <br></br>
-                      <a href="https://lex.uz/ru/docs/5960609?query=%D0%BA%D0%B8%D0%B1%D0%B5%D1%80%D0%B1%D0%B5%D0%B7%D0%BE%D0%BF%D0%B0%D1%81%D0%BD%D0%BE%D1%81%D1%82%D1%8C#sr-1 ">
-                      Ссылка 2
-                      </a>
-                    </p>
-                  </div>
-                  <div className="apply-div">
-                    <p>
-                      Если Вам нужно будет проходить сертификацию в Центре
-                      Кибербезопасности, то для наших резидентов есть льготы в
-                      этом плане. Мы предоставляем резидентам услугу подготовки
-                      к сертификации в виде экспертизы исходных кодов.
-                    </p>
-                  </div>
-                </div>
-                <div className="button-add">
-                  <Button type="submit" className="add-btn" onClick={handleSubmit} variant="contained">
-                    <i className="lni lni-upload"></i>Отправить
-                  </Button>
-                </div>
-              </FormControl>
-            </div>
+              <div className="button-add">
+                <Button type="submit" className="add-btn" onClick={handleSubmit} variant="contained">
+                  <i className="lni lni-upload"></i>Отправить
+                </Button>
+              </div>
+            </FormControl>
           </div>
         </div>
       </div>
-      <Toaster richColors position="bottom-right" />
-    </>
+    </div>
+    <Toaster richColors position="bottom-right" />
+  </>):(<SuccessMessage/>)
+    
   );
 }
